8ffbe1fbd6b632b9aed63083e1d4ea506b20fe93
enoskyeza <enoskyeza@gmail.com>
2023-02-22 10:13:22 +0300

    modified tf page

---
+++ js/script.js
@@ -0,0 +1,28 @@
+(function ($) {
+    "use strict";
+    // Services slider
+    var slider = tns({
+        container: '.my-slider',
+        items: 2,
+        speed: 700,
+        autoplay: true,
+        nav: false,
+        navPosition: 'bottom',
+        autoplayButtonOutput: false,
+        controlsContainer: '#controls',
+        prevButton: '.previous',
+        nextButton: '.next',
+        responsive: {
+            640: {
+              items: 3
+            },
+            700: {
+              gutter: 0
+            },
+            900: {
+              items: 6
+            }
+          }
+      });
+
+})(jQuery);
